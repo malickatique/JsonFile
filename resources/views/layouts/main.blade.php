be235f355b7c80f0b0b32eb990892ce128314e09
malickateeq <malickateeq@gmail.com>
2019-12-10 16:44:01 +0500

    init

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -130,9 +130,9 @@
                   <div class="footer-newsletter">
                     <h4>Our Newsletter</h4>
                     <p>{{ $data['content'][0]->p }}</p>
-                    <form action="#" method="">
+                    <!-- <form action="#" method="">
                       <input type="email" name="email"><input type="submit"  value="Subscribe">
-                    </form>
+                    </form> -->
                   </div>
 
                 </div>
@@ -149,20 +149,7 @@
                     </ul>
                   </div>
 
-                  <div class="footer-links">
-                    <h4>Contact Us</h4>
-                    <p>{{ $data['site']->site_address }} <br>
-                      <strong>Phone:</strong> {{ $data['site']->site_phone }} <br>
-                      <strong>Email:</strong> {{ $data['site']->site_email }}<br>
-                    </p>
-                  </div>
 
-                  <div class="social-links">
-                    <a href="{{ $data['site']->site_twitter }}" class="twitter"><i class="fa fa-twitter"></i></a>
-                    <a href="{{ $data['site']->site_facebook }}" class="facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="{{ $data['site']->site_instagram }}" class="instagram"><i class="fa fa-instagram"></i></a>
-                    <a href="{{ $data['site']->site_linkedin }}" class="linkedin"><i class="fa fa-linkedin"></i></a>
-                  </div>
 
                 </div>
 
@@ -172,33 +159,19 @@
 
           <div class="col-lg-6">
 
-            <div class="form">
-              
-              <h4>Send us a message</h4>
-              <p>Eos ipsa est voluptates. Nostrum nam libero ipsa vero. Debitis quasi sit eaque numquam similique commodi harum aut temporibus.</p>
-              <form action="#" method="" role="form" class="contactForm">
-                <div class="form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validation"></div>
-                </div>
+            <div class="footer-links">
+              <h4>Contact Us</h4>
+              <p>{{ $data['site']->site_address }} <br>
+                <strong>Phone:</strong> {{ $data['site']->site_phone }} <br>
+                <strong>Email:</strong> {{ $data['site']->site_email }}<br>
+              </p>
+            </div>
 
-                <div id="sendmessage">Your message has been sent. Thank you!</div>
-                <div id="errormessage"></div>
-
-                <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
-              </form>
+            <div class="social-links">
+              <a href="{{ $data['site']->site_twitter }}" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="{{ $data['site']->site_facebook }}" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="{{ $data['site']->site_instagram }}" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="{{ $data['site']->site_linkedin }}" class="linkedin"><i class="fa fa-linkedin"></i></a>
             </div>
 
           </div>
